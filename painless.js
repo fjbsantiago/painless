d15e7fc80d54be6a049ae7fef8a23eababdc14f2
Francisco Santiago <fjbsantiago@gmail.com>
2017-07-25 14:54:18 +0200

    Multi-line multi-statement painless script

--- painless.js
+++ painless.js
@@ -10,3 +10,16 @@
         "lang": "painless"
     }
 }
+
+// Multi-statement script
+{
+    "query": {
+        "match": {
+            "city": "Aesmsterdam"
+        }
+    },
+    "script": {
+        "inline": "ctx._source.city = \"Amsterdam\"; ctx._source.zip = \"1070 HL\";",
+        "lang": "painless"
+    }
+}
